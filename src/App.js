317e6d36b139a611676389e68bbc3d264afe642d
OloladeB <banjoelizabeth2019@gmail.com>
2022-09-25 22:23:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import drawers from "./images/drawers.jpg";
+import avatar from "./images/avatar-michelle.jpg";
+import share from "./images/icon-share.svg";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="container">
+      <div className="img-section">
+        <img src={drawers} alt="drawers" className="drawers" />
+      </div>
+      <div className="contents">
+        <h2 className="text1">
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h2>
+        <h3 className="text2">
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I've got some simple tips to help
+          you make any room feel complete.
+        </h3>
+      
+      <div className="bottom">
+        <img src={avatar} alt="michelle" className="avatar-mich" />
+        <p className="texts">
+          <h3 className="text3">Michelle Appleton</h3>
+          <h4 className="text4">28 June 2020</h4>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <img src={share} alt="share-logo" className="share-btn" />
+      </div>
+      </div>
+    </section>
   );
 }
 
